3d2ecfe7f4a11697e558fac55eeefc4c8454a067
rugieri <rugieri.ronaldo@gmail.com>
2021-05-11 23:50:24 -0300

    added logs component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
+import Logs from './components/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -12,6 +13,9 @@ const App = () => {
   return (
     <Fragment>
       <SearchBar />
+      <div className = 'container'>
+        <Logs />
+      </div>
     </Fragment>
   )
 };
