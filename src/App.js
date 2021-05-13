28910663bc41c20cd33646ce3e8a01f846d3c984
rugieri <rugieri.ronaldo@gmail.com>
2021-05-13 17:32:25 -0300

    Added Edit & Tech modal components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
 import AddLogModal from './components/logs/AddLogModal';
-
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/techs/AddTechModal';
+import TechListModal from './components/techs/TechListModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -19,6 +21,9 @@ const App = () => {
       <div className = 'container'>
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
