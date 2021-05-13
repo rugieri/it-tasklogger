ecd94df10c4d4579bedb607f6dacff0e0e2aa6f4
rugieri <rugieri.ronaldo@gmail.com>
2021-05-12 21:35:07 -0300

    Added buttons to add techs and see list of techs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Fragment, useEffect } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -14,6 +16,7 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className = 'container'>
+        <AddBtn />
         <Logs />
       </div>
     </Fragment>
