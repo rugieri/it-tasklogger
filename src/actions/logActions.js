f913b0ac16b3a154036f454e7eca5f12a22bd8a7
rugieri <rugieri.ronaldo@gmail.com>
2021-05-16 18:16:21 -0300

    Connected Redux to component "Logs"

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,5 +1,6 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 
+// Get logs from server
 export const getLogs = () => async dispatch => {
     try {
         setLoading();
--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -1,26 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
 import LogItem from './LogItem';
 import Preloader from '../layout/Preloader';
+import PropTypes from 'prop-types';
+import { getLogs } from '../../actions/logActions'
 
-const Logs = () => {
-    const [logs, setLogs] = useState([]);
-    const [loading, setLoading] = useState(false);
-    
+const Logs = ({ log: { logs, loading }, getLogs}) => {
+        
     useEffect(() => {
         getLogs();
         // eslint-disable-next-line
     }, []);
 
-    const getLogs =async () => {
-        setLoading(true);
-        const res =  await fetch('/logs');
-        const data = await res.json();
-
-        setLogs(data);
-        setLoading(false);
-    };
-    if(loading) {
-        return <Preloader />
+    
+    
+    if(loading  || logs === null) {
+        return <Preloader />;
     }
     return (
         <ul className="collection with-header">
@@ -34,6 +29,19 @@ const Logs = () => {
             )}
          </ul>
     )
+ };
+
+Logs.propTypes = {
+    log: PropTypes.object.isRequired
 }
 
-export default Logs
+const mapStateToProps = state => ({
+    log: state.log
+    
+
+});
+
+export default connect
+(mapStateToProps, 
+    { getLogs}
+    )(Logs);
